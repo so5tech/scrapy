305bc7add769bc8e3a9fa3b8c8e95886e024fcbc
Sourabh Singh <65441264+so5tech@users.noreply.github.com>
2025-12-02 12:11:04 +0530

    cors updated

--- app/main.py
+++ app/main.py
@@ -3,12 +3,20 @@ from fastapi.responses import JSONResponse, FileResponse
 import subprocess
 import os
 from pymongo import MongoClient
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="FastAPI + Scrapy + MongoDB",
     version="1.0.0",
 )
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],  allow_headers=["*"],
+)
 # --------------------------
 # MongoDB Connection
 # --------------------------
